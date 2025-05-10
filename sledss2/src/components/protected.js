b94d684d5f8826d11b2c9c5638fe33660e574b4b
unknown <you@example.com>
2025-05-10 12:14:56 +0300

    updated protected

--- sledss2/src/components/protected.js
+++ sledss2/src/components/protected.js
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const userID = localStorage.getItem("userID"); // Check if user is logged in
 
-  return userID ? children : <Navigate to="/home" replace />; // Redirect if not logged in
+  return userID ? children : <Navigate to="http://elderchatgpt.com:5018/" replace />; // Redirect if not logged in
 };
 
 export default ProtectedRoute;
